b8cb3eb3c69c272e18d35b7abe8274105b50dc81
Alzevir Maicá <alzevir.maica@atlastechnol.com>
2025-09-18 23:31:22 -0300

    first commit

---
+++ rotas/src/pages/Produto/index.js
@@ -0,0 +1,13 @@
+import { useParams } from "react-router-dom";
+
+function Produto() {
+  const { id } = useParams();
+  return (
+    <div>
+      <h2>Detalhe do Produto</h2>
+      <span>Meu produto é {id}</span>
+    </div>
+  );
+}
+
+export default Produto;
