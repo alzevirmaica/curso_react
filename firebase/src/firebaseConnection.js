5dc4731b7b6eee5a7a76c18b7b1d35ae96c74833
Alzevir Maicá <alzevir.maica@atlastechnol.com>
2025-09-23 22:37:36 -0300

    configurando firebase

---
+++ firebase/src/firebaseConnection.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDlJ0GLt5UTVLU251_9d8evJc1M54JNhG4",
+  authDomain: "curso-dd2c2.firebaseapp.com",
+  projectId: "course-dd2c2",
+  storageBucket: "curso-dd2c2.firebasestorage.app",
+  messagingSenderId: "438959230576",
+  appId: "1:438959230576:web:31ae7df31b1fa4d94caeb5",
+  measurementId: "G-Z46B1D9F46",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const db = getFirestore(firebaseApp);
+
+export { db };
