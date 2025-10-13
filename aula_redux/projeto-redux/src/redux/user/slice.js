61d1f3628477ebf3069e1382a22671b464619fc5
Alzevir Maicá <alzevir.maica@atlastechnol.com>
2025-10-13 20:55:06 -0300

    projeto com redux

---
+++ aula_redux/projeto-redux/src/redux/user/slice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: {
+    user: null,
+  },
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    createUser: (state, action) => {
+      console.log(action.payload);
+
+      return {
+        ...state,
+        user: {
+          name: action.payload.name,
+          email: action.payload.email,
+          address: null,
+        },
+      };
+    },
+  },
+});
+export const { createUser } = userSlice.actions;
+export default userSlice.reducer;
